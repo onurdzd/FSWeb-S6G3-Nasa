c2a5174767f57dea53d6a0e3dff82d8034d58a79
onurd <106928484+onurdzd@users.noreply.github.com>
2023-02-01 19:37:21 +0300

    axios data çekildi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=WO6Dip7hem2eo1Bh97e0XbnWKYr0IvK3Ved1996Q"
+      )
+      .then((response) => {
+        setData({
+          copyright: response.data.copyright,
+          title: response.data.title,
+          date: response.data.date,
+          explanation: response.data.explanation,
+          url: response.data.url,
+        });
+      });
+  }, []);
+
+  const { copyright, date, explanation, title, url } = data;
+
   return (
     <div className="App">
       <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
+        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip
+        edin İyi eğlenceler!
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
